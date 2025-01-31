de86b797d8ee7487254a63ec610ab75166473bd6
MGSimard <mgsimard.dev@gmail.com>
2025-01-31 15:25:44 -0500

    pad

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -10,17 +10,15 @@ export default async function PageSignIn() {
 
   return (
     <main id="sign-in">
-      <Section>
-        <div className="authcard">
-          <div className="authcard-header">
-            <h1>Sign In</h1>
-            <p className="detail">Authenticate with your preferred option below.</p>
-          </div>
-          <div className="authcard-content">
-            <SignInButtons />
-          </div>
+      <div className="authcard">
+        <div className="authcard-header">
+          <h1>Sign In</h1>
+          <p className="detail">Authenticate with your preferred option below.</p>
         </div>
-      </Section>
+        <div className="authcard-content">
+          <SignInButtons />
+        </div>
+      </div>
     </main>
   );
 }
